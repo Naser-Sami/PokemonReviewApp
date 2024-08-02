64f29b22293cfbba80e21eb47a859526f2b39854
Naser Sami <naser_ebedo@icloud.com>
2024-08-02 17:53:23 +0300

    OwnerController

---
+++ PokemonReviewApp/Controllers/OwnerController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+
+namespace PokemonReviewApp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class OwnerController : Controller
+	{
+		private readonly IOwnerRepository _ownerRepository;
+		private readonly IMapper _mapper;
+
+		public OwnerController(IOwnerRepository ownerRepository, IMapper mapper)
+		{
+			_ownerRepository = ownerRepository;
+			_mapper = mapper;
+		}
+
+		[HttpGet()]
+		public IActionResult GetOwners()
+		{
+			if (!ModelState.IsValid) return BadRequest();
+			return Ok(_mapper.Map<List<OwnerDto>>
+                (_ownerRepository.GetOwners()));
+        }
+
+		[HttpGet("GetOwner/{ownerId}")]
+		public IActionResult GetOwner(int ownerId)
+		{
+			if (!_ownerRepository.OwnerExists(ownerId))
+				return NotFound();
+			if (!ModelState.IsValid)
+				return BadRequest();
+			
+			return Ok(_mapper.Map<OwnerDto>
+				(_ownerRepository.GetOwner(ownerId)));
+		}
+
+		[HttpGet("GetOwnerOfAPokemno/{pokeId}")]
+		public IActionResult GetOwnerOfAPokemno(int pokeId)
+		{
+            if (!_ownerRepository.PokemonExists(pokeId))
+                return NotFound();
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            return Ok(_mapper.Map<List<OwnerDto>>
+				(_ownerRepository.GetOwnerOfAPokemno(pokeId)));
+        }
+
+		[HttpGet("GetPokemonByOwner/{ownerId}")]
+		public IActionResult GetPokemonByOwner(int ownerId)
+		{
+            if (!_ownerRepository.OwnerExists(ownerId))
+                return NotFound();
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+			return Ok(_mapper.Map<List<PokemonDto>>
+				(_ownerRepository.GetPokemonByOwner(ownerId)));
+        }
+    }
+}
+
---
+++ PokemonReviewApp/Repository/OwnerRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+	public class OwnerRepository : IOwnerRepository
+    {
+        private readonly DataContext _context;
+
+        public OwnerRepository(DataContext context)
+		{
+            this._context = context;
+        }
+
+        public Owner? GetOwner(int ownerId)
+            => _context.Owners.Where(o => o.Id == ownerId).FirstOrDefault();
+
+        public ICollection<Owner> GetOwnerOfAPokemno(int pokeId)
+            => _context.PokemonOwners
+            .Where(p => p.PokemonId == pokeId)
+            .Select(o => o.Owner)
+            .ToList();
+
+        public ICollection<Owner> GetOwners()
+            => _context.Owners
+            .OrderBy(o => o.Id)
+            .ToList();
+
+        public ICollection<Pokemon> GetPokemonByOwner(int ownerId)
+            => _context.PokemonOwners
+            .Where(o => o.OwnerId == ownerId)
+            .Select(p => p.Pokemon)
+            .ToList();
+
+        public bool OwnerExists(int ownerId)
+            => _context.Owners
+            .Any(o => o.Id == ownerId);
+
+        public bool PokemonExists(int pokeId)
+            => _context.Pokemons
+            .Any(p => p.Id == pokeId);
+    }
+}
+
